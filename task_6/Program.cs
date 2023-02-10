85611e350c0c444df6947a51b3a11b7b476eff5b
Alhafat <vladimir-lukin@inbox.ru>
2023-02-10 16:21:56 +0300

    task_6

---
+++ task_6/Program.cs
@@ -0,0 +1,33 @@
+﻿// Задача со звездочкой:
+// Написать функцию Sqrt(x) - квадратного корня, которая принимает на вход целочисленное значение x и возвращает целую часть квадратного корня от введенного числа.
+// 4 -> 2
+// 28 -> 5
+
+// Нельзя использовать встроенные функции библиотеки Math!
+
+
+
+
+int GetMathSqrt(int number)
+{
+    int count = 0;
+    while (true)
+    {
+        if (((number - (count * count)) < count))
+        {
+            System.Console.WriteLine($"Целая часть квадратного корня от числа {number} --> {count:f0}");
+            break;
+        }
+        else count++;
+    }
+    return number;
+}
+
+void main()
+{
+    System.Console.WriteLine("Введите число:");
+    int number = int.Parse(Console.ReadLine()!);
+    GetMathSqrt(number);
+}
+
+main();
